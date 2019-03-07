887d81fae120b3c9f50547af2cc86cef4c22bf24
Park Hyunwoo <ez.amiryo@gmail.com>
2019-03-08 02:57:05 +0900

    Use the first argument as the port number

--- shs.go
+++ shs.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
+    port := "5000"
+
+    if len(os.Args) > 1 {
+        port = os.Args[1]
+    }
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         log.Println("-", r.URL.Path)
         http.ServeFile(w, r, r.URL.Path[1:])
     })
 
-    log.Println("Serving HTTP on http://localhost:5000")
-    log.Fatal(http.ListenAndServe(":5000", nil))
+    log.Println(fmt.Sprintf("Serving HTTP on http://localhost:%s", port))
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
