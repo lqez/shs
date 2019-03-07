f7635edcc7e8a1d0af75cff9df166c40bb299ea6
Park Hyunwoo <ez.amiryo@gmail.com>
2019-03-08 03:06:39 +0900

    Custom 404 Not Found page

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ $ go build
 $ go install
 ```
 
-# Run
+# Usage
+
+### Basic run
 
 ```
 $ shs
 ```
-
-# Arguments
 
 ### Set port
 
@@ -22,6 +22,11 @@ $ shs
 $ shs 4000
 2019/03/08 02:56:30 Serving HTTP on http://localhost:4000
 ```
+
+### Custom 404 page
+
+If `404.html` file exist, shs will serve it as a custom `404 Not Found` response.
+
 
 # License
 
--- shs.go
+++ shs.go
@@ -9,14 +9,20 @@ import (
 
 func main() {
     port := "5000"
+    notFoundPage := "404.html"
 
     if len(os.Args) > 1 {
         port = os.Args[1]
     }
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        log.Println("-", r.URL.Path)
-        http.ServeFile(w, r, r.URL.Path[1:])
+        if _, err := os.Stat("/path/to/whatever"); err == nil {
+            log.Println("-", r.URL.Path)
+            http.ServeFile(w, r, r.URL.Path[1:])
+        } else if os.IsNotExist(err) {
+            log.Println("- Not found -", r.URL.Path)
+            http.ServeFile(w, r, notFoundPage)
+        }
     })
 
     log.Println(fmt.Sprintf("Serving HTTP on http://localhost:%s", port))
